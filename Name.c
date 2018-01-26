c16822c947a1fac728885cef77069ac6693cf219
Trevor Nichelson <trevor.nichelson@huskers.unl.edu>
2018-01-26 16:58:26 -0600

    Name.c
    This is a hello world program with my name.

---
+++ Name.c
@@ -0,0 +1,16 @@
+/**
+*Author: Trevor Nichelson 
+*Date: 01/25/2018
+*
+* This program prints my name.
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+	printf("Trevor Nichelson!\n");
+
+	return 0;
+}
